77f95bf3661934ef1670bed9fe34ac78d4a5aab6
= <=>
2019-02-25 14:51:05 +0700

    file

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blog = Blog::all();
+        return view('list', compact('blog'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('insert_form');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $blog = new Blog();
+        $blog->title = $request->input('title');
+        $blog->bai_viet = $request->input('text');
+        $blog->save();
+        return redirect()->route('index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $edit = Blog::find($id);
+        return view('edit', compact('edit', 'id'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $edit = Blog::find($id);
+        $edit->bai_viet = $request->input('edit');
+        $edit->save();
+        return redirect()->route('index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $delete = Blog::find($id);
+        $delete->delete();
+        return redirect()->route('index');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/index', 'BlogController@index')->name('index') ;
+
+Route::get('/create', 'BlogController@create')->name('create');
+Route::post('/create', 'BlogController@store')->name('store');
+
+Route::get('{id}/edit', 'BlogController@edit')->name('edit');
+Route::post('{id}/edit', 'BlogController@update')->name('update');
+
+Route::get('{id}/destroy', 'BlogController@destroy')->name('destroy');
